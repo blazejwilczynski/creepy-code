507c8b61ac2b1e612eca54051a52877aea673d12
Blazej Wilczynski <blazej.wilczynski@gmail.com>
2025-10-15 23:06:44 +0200

    Landing working on

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,17 @@
 import './styles/Landing.css'
+import upsdie_face from '../assets/emoji/upside_down_face.png'
 
 export function Landing() {
     return (
         <div className="landing">
-        <div className="blob"></div>
+            <div className="landing-wrapper">
+                <div className="emoji-icons">
+                    <div className="emoji row"></div>
+                    <div className="emoji row"></div>
+                    <div className="emoji row"></div>
+                </div>
+                <div className="landing-text"></div>
+            </div>
         </div>
     )
 }
